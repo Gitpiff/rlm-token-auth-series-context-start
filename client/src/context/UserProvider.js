90f7515e3db417c1e540cde9866061bdc125674f
Victor <viknavarro@gmail.com>
2022-08-10 09:33:39 -0500

    Update UserProvider.js

--- client/src/context/UserProvider.js
+++ client/src/context/UserProvider.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react"
+import axios from "axios"
 export const UserContext = React.createContext()
 
 
 export default function UserProvider(props){
-  const initState = { user: {}, token: ""}
+  const initState = { user: {}, token: "", todos: []}
 
   const [userState, setUserState] = useState(initState)
+
+  //signup fx, we need the credentials as an argument -props-
+  function signup(credentials){
+    axios.post("/auth/signup", credentials)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
   return (
     <UserContext.Provider
       value={{
-        ...userState
+        ...userState,
+        signup
       }}
     >
       {props.children}
