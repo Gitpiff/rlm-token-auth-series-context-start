ff0c500b166a656d027ad3ad49f89277cd1653ee
Victor <viknavarro@gmail.com>
2022-08-11 11:27:04 -0500

    added hashed passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require("bcrypt")
 
 const userSchema = new Schema({
   username: {
@@ -21,5 +22,36 @@ const userSchema = new Schema({
     default: false
   }
 })
+
+//pre-save hook to encrypt passwords on signup
+//do not use arrow functions, use the entire syntax for the function, because we need access to the keyword "this", without the word "function" we don't have the context/access
+userSchema.pre("save", function(next){
+  const user = this
+  if(!user.isModified("password")) return next()
+  //takes two arguments, what are we hashing and salt rounds
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if(err) return next(err)
+    user.password = hash
+    next()
+  })
+})
+
+
+//method to check encrypted password on login
+//the function takes two arguments, a string and a function
+userSchema.methods.checkPassword = function(passwordAttempt, callback){
+  bcrypt.compare(passwordAttempt, this.password, (err, isMatch) => {
+    if(err) return callback(err)
+    return callback(null, isMatch)
+  })
+}
+
+//method to remove user's password for token/sending the response
+//this function does not expect any parameters
+userSchema.methods.withoutPassword = function(){
+  const user = this.toObject()
+  delete user.password
+  return user
+}
 
 module.exports = mongoose.model("User", userSchema)
